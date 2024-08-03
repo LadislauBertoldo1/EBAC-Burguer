eb0782b13aae0a3b3623676cb22d4a86bd5c9b4b
Ladislau Bertoldo <ladislau.bertoldo1997@hotmail.com>
2024-08-03 19:18:35 -0300

    first Commit

---
+++ Jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.menu-promocao').click(function() {
+        $('.submenu-promocao').slideToggle();
+    })
+})
+
+$(document).ready(function() {
+    $('.menu-hamburguer').click(function() {
+        $('.submenu-hamburguer').slideToggle();
+    })
+})
+
+$(document).ready(function() {
+    $('.menu-sobremesa').click(function() {
+        $('.submenu-sobremesa').slideToggle();
+    })
+})
+
